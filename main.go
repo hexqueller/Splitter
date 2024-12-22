0e5519a6e5622547aa9588dfee267b489df42949
hexqueller <dima.nesed@gmail.com>
2024-12-22 15:21:38 +0300

    New func - Divisors and SelectFromArray

--- main.go
+++ main.go
@@ -1,14 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-func NumIsEven(number int64) bool {
-	return number%2 == 0
+func Divisors(num int64) []int64 {
+	var divisors []int64
+	for i := int64(1); i*i <= num; i++ {
+		if num%i == 0 {
+			divisors = append(divisors, i)
+			if i != num/i {
+				divisors = append(divisors, num/i)
+			}
+		}
+	}
+
+	sort.Slice(divisors, func(i, j int) bool {
+		return divisors[i] < divisors[j]
+	})
+
+	return divisors
+}
+
+func SelectFromArray(options []int64) int64 {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Select a number from the list:")
+		for i, option := range options {
+			fmt.Printf("%d: %d\n", i+1, option)
+		}
+
+		fmt.Print("Choice: ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Input error, try again.")
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+
+		choice, err := strconv.Atoi(input)
+		if err != nil || choice < 1 || choice > len(options) {
+			fmt.Println("Wrong number, try again.")
+			continue
+		}
+
+		return options[choice-1]
+	}
 }
 
 func main() {
@@ -41,6 +88,6 @@ func main() {
 	fmt.Printf("File: %s\n", filename)
 	fmt.Printf("Size: %d byte\n", size)
 
-	numOfcopyes := NumIsEven(size)
-	fmt.Printf("Number is Even : %t\n", numOfcopyes)
+	choice := SelectFromArray(Divisors(size))
+	fmt.Println(choice)
 }
